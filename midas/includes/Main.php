402551365eaa77bf0985a186e6fbb3f1cfc61c16
Brandon Tassone <brandontassone@gmail.com>
2017-02-01 15:16:49 -0500

    Updated the Main class as far as I could. Time to tackle the loaders and admin/public facing classes

--- midas/includes/Main.php
+++ midas/includes/Main.php
@@ -8,38 +8,44 @@ class Main {
 	protected $version;
 
 	public function __construct() {
+		$this->plugin_name = "Midas";
+		$this->version = "1.0.0";
 
+		$this->load_dependencies();
+		$this->set_locale();
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
 	}
 
 	public function get_loader() {
-
+		return $this->loader;
 	}
 
 	public function get_plugin_name() {
-
+		return $this->plugin_name;
 	}
 
 	public function get_version() {
-
+		return $this->version;
 	}
 
 	public function run() {
-
+		// Run
 	}
 
 	private function define_admin_hooks() {
-
+		// Midas admin wrap stuff goes here.
 	}
 
 	private function define_public_hooks() {
-
+		// Public facing template goes here.
 	}
 
 	private function load_dependencies() {
-
+		// Loader logic and wrap here
 	}
 
 	private function set_locale() {
-
+		// Language classes go here
 	}
 }

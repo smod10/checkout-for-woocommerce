81475bbdc0ba2f6e4203d5a109b46ce8d752c492
Brandon Tassone <brandontassone@gmail.com>
2017-02-01 14:12:37 -0500

    Modified the .gitignore to be a bit more robust. Added in plugin boilerplate (https://wppb.me/)

---
+++ midas/includes/class-midas-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       brandont.me
+ * @since      1.0.0
+ *
+ * @package    Midas
+ * @subpackage Midas/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Midas
+ * @subpackage Midas/includes
+ * @author     Brandon Tassone <brandontassone@gmail.com>
+ */
+class Midas_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ midas/includes/class-midas-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       brandont.me
+ * @since      1.0.0
+ *
+ * @package    Midas
+ * @subpackage Midas/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Midas
+ * @subpackage Midas/includes
+ * @author     Brandon Tassone <brandontassone@gmail.com>
+ */
+class Midas_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}

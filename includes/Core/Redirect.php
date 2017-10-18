d554b8f949ad85e3f06c0f949aff866f389f4536
Clifton Griffin <clifgriffin@gmail.com>
2017-10-18 15:10:11 -0400

    Redirect to cart with error if empty cart.

--- includes/Core/Redirect.php
+++ includes/Core/Redirect.php
@@ -16,6 +16,13 @@ class Redirect {
 	    // If using our checkout system force password generation for new users.
 		update_option('woocommerce_registration_generate_password', 'yes');
 
+		if ( WC()->cart->is_empty() ) {
+			// When on the checkout with an empty cart, redirect to cart page
+			wc_add_notice( __( 'Checkout is not available whilst your cart is empty.', 'woocommerce' ), 'notice' );
+			wp_redirect( wc_get_page_permalink( 'cart' ) );
+			exit;
+		}
+
 		if ( ( $settings_manager->get_setting('enable') == 'yes' || current_user_can('manage_options') ) && function_exists('is_checkout') && is_checkout() && !is_order_received_page() ) {
 			// Allow global parameters accessible by the templates
 			$global_template_parameters = apply_filters('cfw_template_global_params', array());

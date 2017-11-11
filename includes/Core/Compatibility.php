5a27aaadf0cb364bb7d9f5400c214aded37c1938
Clifton Griffin <clifgriffin@gmail.com>
2017-11-11 10:43:35 -0500

    Render form fields and include stylesheet / js.
    Honestly, this may be an impossible task as we have so much to consider on form field transmission, validation, total updates, etc. Not sure if this one is possible.

--- includes/Core/Compatibility.php
+++ includes/Core/Compatibility.php
@@ -12,7 +12,15 @@ namespace Objectiv\Plugins\Checkout\Core;
  */
 class Compatibility {
 	public function __construct() {
-		add_action('cfw_wp_head', array($this, 'mixpanel_fixes') );
+		// MixPanel Compatibility
+	    add_action('cfw_wp_head', array($this, 'mixpanel_fixes') );
+
+		// Checkout Add-ons Compatibility
+        add_filter('wc_checkout_add_ons_position', array($this, 'set_checkout_add_ons_position') );
+
+        // Allow scripts and styles for certain plugins
+        add_filter('cfw_allowed_script_handles', array($this, 'allowed_scripts') );
+        add_filter('cfw_allowed_style_handles', array($this, 'allowed_styles') );
 	}
 
 	function mixpanel_fixes() {
@@ -44,4 +52,26 @@ class Compatibility {
 		</script>
 		<?php
 	}
+
+	function set_checkout_add_ons_position() {
+        return 'cfw_checkout_before_payment_method_terms_checkbox';
+    }
+
+    function allowed_scripts( $scripts ) {
+	    // Jilt
+        $scripts[] = 'wc-jilt';
+
+        // Checkout Add-ons
+	    $scripts[] = 'plupload-all';
+	    $scripts[] = 'wc-checkout-add-ons-frontend';
+
+	    return $scripts;
+    }
+
+    function allowed_styles( $styles ) {
+	    // Checkout Add-ons
+	    $styles[] = 'wc-checkout-add-ons-frontend';
+
+	    return $styles;
+    }
 }

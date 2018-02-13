fc80a43975358416f34adcebfd2248821d8a5274
Clifton Griffin <clifgriffin@gmail.com>
2018-02-13 14:47:31 -0500

    Add support for PayPal Express button on checkout. Frame out support for apple pay (but it doesn't seem to exist really)

--- includes/Core/Compatibility.php
+++ includes/Core/Compatibility.php
@@ -65,6 +65,60 @@ class Compatibility {
 				add_action( 'cfw_wp_footer', array( 'AEPC_Pixel_Scripts', 'pixel_init' ), 1 );
 			}
 		}
+
+		/**
+         * One Click Upsells
+         */
+		if ( defined('GB_OCU_VER') ) {
+			add_action('wp', array($this, 'add_ocu_checkout_buttons' ) );
+        }
+	}
+
+	function add_ocu_checkout_buttons() {
+		$gateways = WC()->payment_gateways->get_available_payment_gateways();
+		$add_sep = false;
+
+		if ( ! empty( $gateways['ocustripe'] ) ) {
+		    if ( $gateways['ocustripe']->apple_pay_enabled != 'no' ) {
+			    add_action( 'cfw_checkout_before_customer_info_tab', 'gb_ocu_stripe_apple_pay_display_button', 5);
+
+			    $add_sep = true;
+            }
+        }
+
+        if ( ! empty( $gateways[ 'ocupaypal' ] ) ) {
+		    if ( $gateways['ocupaypal']->checkout_page == 'top' || $gateways['ocupaypal']->checkout_page == 'both' ) {
+			    add_action( 'cfw_checkout_before_customer_info_tab', array($this, 'gb_ocu_paypal_display_button'), 5 );
+
+			    $add_sep = true;
+            }
+        }
+
+        if ( $add_sep ) {
+	        if ( ! has_action('cfw_checkout_before_customer_info_tab', array($this, 'add_separator') ) ) {
+		        add_action('cfw_checkout_before_customer_info_tab', array($this, 'add_separator'), 10);
+	        }
+        }
+    }
+
+	function gb_ocu_paypal_display_button() {
+		$gateways = WC()->payment_gateways->get_available_payment_gateways();
+
+		if(
+			! empty( $gateways[ 'ocupaypal' ] ) &&
+			method_exists( $gateways[ 'ocupaypal' ], 'paypal_display_button' )
+		)
+		{
+			$checkout_page = $gateways[ 'ocupaypal' ]->checkout_page;
+
+			if( $checkout_page == 'top' || $checkout_page == 'both' ) {
+				echo '<div class="woocommerce-info" style="text-align: center;">';
+
+				$gateways[ 'ocupaypal' ]->paypal_display_button();
+
+				echo '</div>';
+			}
+		}
 	}
 
 	function add_pixel_caffeine_body_class( $classes ) {

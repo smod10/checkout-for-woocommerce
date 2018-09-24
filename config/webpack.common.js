6826da4e291f52b642db33268ae6a82daa97a1db
Brandon Tassone <brandontassone@gmail.com>
2018-09-24 04:00:15 -0400

    Removed comment in common config (was already self evident what it does)

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -17,7 +17,6 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
 			{
 				test: /\.ts$/,
 				loader: ['ts-loader']

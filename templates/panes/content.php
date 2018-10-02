7cb07ece532d2c326b5a13982f232f7d65176003
Brandon Tassone <brandontassone@gmail.com>
2018-10-02 18:11:50 -0400

    Move footer into content.php

--- templates/panes/content.php
+++ templates/panes/content.php
@@ -327,6 +327,20 @@
                 </form>
 
 	            <?php do_action('cfw_checkout_after_form'); ?>
+
+                <footer id="cfw-footer">
+                    <div class="wrap">
+                        <div class="cfw-container cfw-column-12">
+                            <div class="cfw-footer-inner">
+					            <?php if ( ! empty( $footer_text = Objectiv\Plugins\Checkout\Main::instance()->get_settings_manager()->get_setting('footer_text') ) ): ?>
+						            <?php echo $footer_text; ?>
+					            <?php else: ?>
+                                    Copyright &copy; <?php echo date("Y"); ?>, <?php echo get_bloginfo('name'); ?>. All rights reserved.
+					            <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
 
                 <!-- Cart / Sidebar Column -->

47759e31772dff5dbc5a9fd2cb3bed2c916ad32c
Brandon Tassone <brandontassone@gmail.com>
2018-10-12 01:31:31 -0400

    These tabs keep messing up somehow. O.o

--- sources/ts/front/CFW/Compatibility/Braintree.ts
+++ sources/ts/front/CFW/Compatibility/Braintree.ts
@@ -27,26 +27,26 @@ export class Braintree extends Compatibility {
 		let easyTabsWrap: any = main.easyTabService.easyTabsWrap;
 
 		if(params.cc_gateway_available) {
-            // Bind to the easytabs after
-            easyTabsWrap.bind('easytabs:after', (event, clicked, target) => this.creditCardPaymentRefreshOnTabSwitch(main, event, clicked, target));
-        }
+			// Bind to the easytabs after
+			easyTabsWrap.bind('easytabs:after', (event, clicked, target) => this.creditCardPaymentRefreshOnTabSwitch(main, event, clicked, target));
+		}
 	}
 
-    /**
+	/**
 	 * The braintree credit card handler needs to be refreshed when switching to the payment tab from another tab otherwise the fields won't re-generate.
 	 *
-     * @param {Main} main
-     * @param {any} event
-     * @param {any} clicked
-     * @param {any} target
-     */
+	 * @param {Main} main
+	 * @param {any} event
+	 * @param {any} clicked
+	 * @param {any} target
+	 */
 	creditCardPaymentRefreshOnTabSwitch(main: Main, event: any, clicked: any, target: any) {
-        let easyTabDirection: EasyTabDirection = EasyTabService.getTabDirection(target);
-        let easyTabID: string = EasyTabService.getTabId(easyTabDirection.target);
-        let paymentContainerId: string = main.tabContainer.tabContainerSectionBy("name", "payment_method").jel.attr("id");
+		let easyTabDirection: EasyTabDirection = EasyTabService.getTabDirection(target);
+		let easyTabID: string = EasyTabService.getTabId(easyTabDirection.target);
+		let paymentContainerId: string = main.tabContainer.tabContainerSectionBy("name", "payment_method").jel.attr("id");
 
-        if(paymentContainerId === easyTabID) {
-            wc_braintree_credit_card_handler.refresh_braintree();
-        }
+		if(paymentContainerId === easyTabID) {
+			wc_braintree_credit_card_handler.refresh_braintree();
+		}
 	}
 }

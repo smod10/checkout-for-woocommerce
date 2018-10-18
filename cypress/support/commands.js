bc8c605f6a5eac9c63a876cbb0c5a6fa3b78d27c
Clifton Griffin <clifgriffin@gmail.com>
2018-10-18 16:07:17 -0400

    More working tests.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -36,3 +36,42 @@ Cypress.Commands.add("add_item_to_cart", () => {
         }
     })
 });
+
+Cypress.Commands.add("fill_customer_information_tab_and_advance", () => {
+    cy.get( '#billing_email' ).then( ($input) => {
+        $input.val( Cypress.env( "billing_email" ) ).change();
+    } );
+    cy.get( '#shipping_first_name' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_first_name" ) )
+    } );
+    cy.get( '#shipping_last_name' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_last_name" ) )
+    } );
+    cy.get( '#shipping_address_1' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_address_1" ) )
+    } );
+    cy.get( '#shipping_address_2' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_address_2" ) )
+    } );
+    cy.get( '#shipping_company' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_company" ) )
+    } );
+    cy.get( '#shipping_country' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_country" ) )
+    } );
+    cy.get( '#shipping_postcode' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_postcode" ) )
+    } );
+    cy.get( '#shipping_state' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_state" ) )
+    } );
+    cy.get( '#shipping_city' ).then( ($input) => {
+        $input.val( Cypress.env( "shipping_city" ) )
+    } );
+
+    cy.get( '#cfw-shipping-info-action .cfw-primary-btn' ).click();
+
+    cy.wait(500); // If we don't do this, it tries the tests before the tab is loaded
+
+    cy.hash().should( 'eq', '#cfw-shipping-method' );
+} );

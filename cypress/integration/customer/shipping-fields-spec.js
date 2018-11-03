747b83c2a662d7280c81e83fc4c80132156e542d
Brandon Tassone <brandontassone@gmail.com>
2018-11-02 20:16:26 -0400

    Changed some things around in the test

--- cypress/integration/customer/shipping-fields-spec.js
+++ cypress/integration/customer/shipping-fields-spec.js
@@ -1,20 +1,22 @@
 import dataScaffolding from "../../data_scaffolding/data-scaffolding";
 
-describe( 'Shipping Field Validation', function() {
+describe( 'Shipping Fields', function() {
     before( function() {
         cy.add_item_to_cart();
         cy.visit('checkout');
     } );
 
-    it( 'Validates required shipping address fields', function() {
-        // Required to test state field
-        cy.get( dataScaffolding.fields.account.email ).type( Cypress.env( "account" ).email );
-        cy.get( '#cfw-shipping-info input[required]').each(($input, index, $lis) => {
-            cy.wrap($input).focus().blur().should( 'have.class', 'parsley-error' );
-        });
+    describe('Validation', () => {
+		it('Validates required shipping address fields', () => {
+			// Required to test state field
+			cy.get( dataScaffolding.fields.account.email ).type( Cypress.env( "account" ).email );
+			cy.get( '#cfw-shipping-info input[required]').each(($input, index, $lis) => {
+				cy.wrap($input).focus().blur().should( 'have.class', 'parsley-error' );
+			});
 
-        cy.get( '#cfw-shipping-info-action .cfw-primary-btn' ).click();
-        cy.get( '#shipping_country' ).should( 'not.have.class', 'parsley-error' );
-        cy.get( '#shipping_state' ).should( 'have.class', 'parsley-error' );
-    } );
+			cy.get( '#cfw-shipping-info-action .cfw-primary-btn' ).click();
+			cy.get( '#shipping_country' ).should( 'not.have.class', 'parsley-error' );
+			cy.get( '#shipping_state' ).should( 'have.class', 'parsley-error' );
+		});
+    });
 });
--- cypress/integration/payment/billing-fields-spec.js
+++ cypress/integration/payment/billing-fields-spec.js
@@ -1,4 +1,4 @@
-describe( 'Billing Field Validation + Billing Zip Autocomplete', function() {
+describe( 'Billing Fields', function() {
     beforeEach( function() {
         cy.add_item_to_cart();
         cy.visit('checkout');
@@ -7,25 +7,29 @@ describe( 'Billing Field Validation + Billing Zip Autocomplete', function() {
         cy.wait(500);
     } );
 
-    it( 'Validates required billing address fields', function() {
-        // Required to test state field
-        cy.get( '#shipping_dif_from_billing' ).check();
-        cy.get( '#cfw-billing-fields-container input[required]').each(($input, index, $lis) => {
-            cy.wrap($input).focus().blur().should( 'have.class', 'parsley-error' );
-        });
+    describe('Validation', () => {
+		it( 'Billing address fields', () => {
+			// Required to test state field
+			cy.get( '#shipping_dif_from_billing' ).check();
+			cy.get( '#cfw-billing-fields-container input[required]').each(($input, index, $lis) => {
+				cy.wrap($input).focus().blur().should( 'have.class', 'parsley-error' );
+			});
 
-        cy.get( '#place_order' ).click();
-        cy.get( '#billing_country' ).should( 'not.have.class', 'parsley-error' );
-        cy.get( '#billing_state' ).should( 'have.class', 'parsley-error' );
-    } );
+			cy.get( '#place_order' ).click();
+			cy.get( '#billing_country' ).should( 'not.have.class', 'parsley-error' );
+			cy.get( '#billing_state' ).should( 'have.class', 'parsley-error' );
+		});
+    });
 
-    it( 'Autocompletes domestic city and state', function() {
-        cy.get( '#shipping_dif_from_billing' ).check();
-        cy.get( '#billing_postcode').type( Cypress.env( "shipping" ).default.postcode );
+    describe('Auto-complete', () => {
+		it( 'Domestic city and state', () => {
+			cy.get( '#shipping_dif_from_billing' ).check();
+			cy.get( '#billing_postcode').type( Cypress.env( "shipping" ).default.postcode );
 
-        cy.wait(1000);
+			cy.wait(1000);
 
-        cy.get( '#billing_state' ).should( 'have.value', Cypress.env( "shipping" ).default.state );
-        cy.get( '#billing_city' ).should( 'have.value', Cypress.env( "shipping" ).default.city );
-    } );
+			cy.get( '#billing_state' ).should( 'have.value', Cypress.env( "shipping" ).default.state );
+			cy.get( '#billing_city' ).should( 'have.value', Cypress.env( "shipping" ).default.city );
+		});
+    });
 });

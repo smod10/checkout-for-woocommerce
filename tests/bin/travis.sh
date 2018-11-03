5eb1065f94d61796572fe754d73801cd7a89e072
Brandon Tassone <brandontassone@gmail.com>
2018-11-02 20:24:09 -0400

    Enable videos and add them to the uploads

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -32,4 +32,13 @@ if [ $1 == 'after' ]; then
 		fi
 	fi
 
+	if [[ ${RUN_E2E} == 1 && $(ls -A $CFW_DIR/cypress/videos) ]]; then
+		if [[ -z "${ARTIFACTS_KEY}" ]]; then
+  			echo "Videos were not uploaded. Please run the e2e tests locally to see failures."
+		else
+  			curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
+			artifacts upload
+		fi
+	fi
+
 fi

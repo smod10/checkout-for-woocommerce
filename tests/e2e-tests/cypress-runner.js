5f927ff2137f68dc9291ef46c8d867bdcb381340
Brandon Tassone <brandontassone@gmail.com>
2018-11-05 23:04:28 -0500

    Updated the then hook to check if totalFailed > 0. If it is we throw an error and in the catch exit with 1

--- tests/e2e-tests/cypress-runner.js
+++ tests/e2e-tests/cypress-runner.js
@@ -4,10 +4,10 @@ const deleteEmpty = require('delete-empty');
 
 cypress.run({
 	spec: [
-		// "cypress/integration/customer/**/*",
-		// "cypress/integration/general/**/*",
+		"cypress/integration/customer/**/*",
+		"cypress/integration/general/**/*",
 		"cypress/integration/shipping/**/*",
-		// "cypress/integration/payment/billing-fields-spec.js"
+		"cypress/integration/payment/billing-fields-spec.js"
 	]
 }).then(results => {
 	// Loop over each run and if the shouldUploadVideo flag is false, delete the video
@@ -16,5 +16,10 @@ cypress.run({
 	// Delete empty directories from the videos folders
 	deleteEmpty("cypress/videos/").catch(console.error);
 
-	return results;
+	// If any of the tests failed throw an error and exit with 1 in the catch
+	if(results.totalFailed > 0) {
+		throw new Error();
+	}
+}).catch(err => {
+	process.exit(1);
 });

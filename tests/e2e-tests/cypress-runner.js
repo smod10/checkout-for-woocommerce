8a164fda24a2eff37591a20cd0bbd0e9e1c49262
Brandon Tassone <brandontassone@gmail.com>
2018-11-05 22:48:07 -0500

    Seeing what happens when results are returned from cypress promise on Travis. Might be enough to see that it fails

--- tests/e2e-tests/cypress-runner.js
+++ tests/e2e-tests/cypress-runner.js
@@ -4,15 +4,17 @@ const deleteEmpty = require('delete-empty');
 
 cypress.run({
 	spec: [
-		"cypress/integration/customer/**/*",
-		"cypress/integration/general/**/*",
+		// "cypress/integration/customer/**/*",
+		// "cypress/integration/general/**/*",
 		"cypress/integration/shipping/**/*",
-		"cypress/integration/payment/billing-fields-spec.js"
+		// "cypress/integration/payment/billing-fields-spec.js"
 	]
-}).then((results) => {
+}).then(results => {
 	// Loop over each run and if the shouldUploadVideo flag is false, delete the video
 	results.runs.forEach(run => (!run.shouldUploadVideo) ? fs.unlinkSync(run.video) : null);
 
 	// Delete empty directories from the videos folders
 	deleteEmpty("cypress/videos/").catch(console.error);
+
+	return results;
 });

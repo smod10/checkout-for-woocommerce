e5a97421118c41e7991d3f78e37a77cb04f0c260
Brandon Tassone <brandontassone@gmail.com>
2018-11-07 04:48:03 -0500

    Add retry attempt

--- tests/e2e-tests/cypress-runner.js
+++ tests/e2e-tests/cypress-runner.js
@@ -2,13 +2,18 @@ const cypress = require('cypress');
 const fs = require('fs');
 const deleteEmpty = require('delete-empty');
 
-cypress.run({
-	spec: [
-		"cypress/integration/customer/**/*",
-		"cypress/integration/general/**/*",
-		"cypress/integration/payment/billing-fields-spec.js"
-	]
-}).then(results => {
+let testAttempts = 0;
+let retryAmt = 1;
+
+let specs = [
+	"cypress/integration/customer/**/*",
+	"cypress/integration/general/**/*",
+	"cypress/integration/payment/billing-fields-spec.js"
+];
+
+let config = { spec: specs };
+
+function deleteSuccessVideos(results) {
 	// Loop over each run and if the shouldUploadVideo flag is false, delete the video
 	results.runs.forEach(run => (!run.shouldUploadVideo) ? fs.unlinkSync(run.video) : null);
 
@@ -19,6 +24,16 @@ cypress.run({
 	if(results.totalFailed > 0) {
 		throw new Error();
 	}
-}).catch(err => {
-	process.exit(1);
-});
+}
+
+function error(err) {
+	if(testAttempts < retryAmt) {
+		testAttempts++;
+		console.log("Attempting tests one more time");
+		cypress.run(config).then(deleteSuccessVideos).catch(error);
+	} else {
+		process.exit(1);
+	}
+}
+
+cypress.run(config).then(deleteSuccessVideos).catch(error);

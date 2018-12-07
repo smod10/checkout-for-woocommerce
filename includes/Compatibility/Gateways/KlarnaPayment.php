6adfeee9885ae78dbe172e000fc7c0a2e4ee089f
Clifton Griffin <clifgriffin@gmail.com>
2018-12-07 14:58:59 -0500

    Working Klarna Payments with patched version of gateway.

--- includes/Compatibility/Gateways/KlarnaPayment.php
+++ includes/Compatibility/Gateways/KlarnaPayment.php
@@ -13,152 +13,90 @@ class KlarnaPayment extends Base {
 	}
 
 	function is_available() {
-		$is_available = false;
-
-		$available_gateways    = WC()->payment_gateways()->get_available_payment_gateways();
-		$this->klarna_payments = $available_gateways['klarna_payments'];
-
-		if ( class_exists( '\\WC_Klarna_Payments' ) && $this->klarna_payments ) {
-			$is_available = true;
-		}
-
-		return $is_available;
-	}
-
-	function typescript_class_and_params( $compatibility ) {
-		$compatibility[] = [
-			'class'  => 'KlarnaPayment',
-			'event'  => 'before-setup',
-			'params' => [],
-		];
-
-		return $compatibility;
+		return class_exists( '\\WC_Klarna_Payments' );
 	}
 
 	function run() {
+		$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
+
+		if ( empty( $available_gateways['klarna_payments'] ) ) {
+			return;
+		}
+
+		$this->klarna_payments = $available_gateways['klarna_payments'];
+
 		add_action( 'cfw_payment_gateway_list_klarna_payments_alternate', array( $this, 'klarna_payments_content' ) );
 		add_filter( 'cfw_show_gateway_klarna_payments', '__return_false' );
 	}
 
 	function klarna_payments_content() {
-		$klarna_dom = new \DOMDocument();
-		$klarna_dom->loadHTML( $this->get_klarna_template() );
-		$li_elements = $klarna_dom->getElementsByTagName( 'li' );
+		do_action( 'klarna_payments_template' );
+		if ( is_array( WC()->session->get( 'klarna_payments_categories' ) ) ) {
+			$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
+			$kp                 = $available_gateways['klarna_payments'];
 
-		for ( $i = 0; $i < $li_elements->count(); $i++ ) {
-			$this->manipulate_li_container( $klarna_dom, $li_elements->item( $i ) );
-		}
+			foreach ( apply_filters( 'wc_klarna_payments_available_payment_categories', WC()->session->get( 'klarna_payments_categories' ) ) as $payment_category ) {
+				$payment_category_id   = 'klarna_payments_' . $payment_category->identifier;
+				$payment_category_name = $payment_category->name;
+				$payment_category_icon = $payment_category->asset_urls->standard;
+				$kp                    = $available_gateways['klarna_payments'];
+				$kp->id                = $payment_category_id;
+				$kp->title             = $payment_category_name;
+				$kp->icon              = $payment_category_icon;
+				$headers               = get_headers( $kp->icon );
+				if ( 'HTTP/1.1 404 Not Found' === $headers[0] ) {
+					switch ( $kp->id ) {
+						case 'klarna_payments_pay_later':
+							$kp->icon = 'https://cdn.klarna.com/1.0/shared/image/generic/badge/sv_se/pay_later/standard/pink.svg';
+							break;
+						case 'klarna_payments_pay_over_time':
+							$kp->icon = 'https://cdn.klarna.com/1.0/shared/image/generic/badge/sv_se/slice_it/standard/pink.svg';
+							break;
+						case 'klarna_payments_pay_now':
+							$kp->icon = 'https://cdn.klarna.com/1.0/shared/image/generic/badge/sv_se/pay_now/standard/pink.svg';
+							break;
+					}
+				}
+				?>
+				<li class="wc_payment_method payment_method_<?php echo $kp->id; ?> cfw-radio-reveal-li">
+					<div class="payment_method_title_wrap cfw-radio-reveal-title-wrap">
+						<label class="payment_method_label cfw-radio-reveal-label" for="payment_method_<?php echo $kp->id; ?>">
+							<input id="payment_method_<?php echo $kp->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $kp->id ); ?>" <?php echo ($count == 0) ? "checked" : ""; ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
+							<span class="payment_method_title cfw-radio-reveal-title"><?php echo $kp->get_title(); ?></span>
+						</label>
 
-		echo $klarna_dom->saveHTML();
-	}
+						<span class="payment_method_icons">
+                            <?php echo $kp->get_icon(); ?>
+                        </span>
+					</div>
+					<?php if ( apply_filters("cfw_payment_gateway_{$kp->id}_content", $kp->has_fields() || $kp->get_description() ) ) : ?>
+						<div class="payment_box_wrap cfw-radio-reveal-content-wrap" <?php if ( ! $kp->chosen ) : ?>style="display:none;"<?php endif; ?>>
+							<div class="payment_box payment_method_<?php echo $kp->id; ?> cfw-radio-reveal-content">
+								<?php
+								ob_start();
+								$kp->payment_fields();
 
-	/**
-	 * @param \DOMDocument $dom
-	 * @param \DOMElement $li
-	 */
-	function manipulate_li_container( $dom, $li ) {
+								$field_html = ob_get_clean();
 
-		// Regex patterns
-		$klarna_data_pattern           = '/(klarna_payments)_{1}[a-z_-]*/';
-		$klarna_payment_method_pattern = '/(payment_method_klarna_payments)_{1}[a-z_-]*/';
-		$klarna_payment_box_pattern    = '/payment_box/';
+								/**
+								 * Gateway Compatibility Patches
+								 */
+								// Expiration field fix
+								$field_html = str_ireplace('js-sv-wc-payment-gateway-credit-card-form-expiry', 'js-sv-wc-payment-gateway-credit-card-form-expiry  wc-credit-card-form-card-expiry', $field_html);
+								$field_html = str_ireplace('js-sv-wc-payment-gateway-credit-card-form-account-number', 'js-sv-wc-payment-gateway-credit-card-form-account-number  wc-credit-card-form-card-number', $field_html);
 
-		// Get item references
-		$inputs               = $li->getElementsByTagName( 'input' );
-		$payment_method_icons = $dom->getElementsByTagName( 'img' );
-		$divs                 = $dom->getElementsByTagName( 'div' );
-		$labels               = $dom->getElementsByTagName( 'label' );
+								// Credit Card Field Placeholders
+								$field_html = str_ireplace('•••• •••• •••• ••••', 'Card Number', $field_html);
+								$field_html = str_ireplace('&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;', 'Card Number', $field_html);
 
-		// Add the reveal class
-		$li->setAttribute( 'class', $li->getAttribute( 'class' ) . ' cfw-radio-reveal-li' );
-
-		// Create the wrapper divs
-		$title_bar = $dom->createElement( 'div' );
-		$title_bar->setAttribute( 'class', 'payment_method_title_wrap cfw-radio-reveal-title-wrap' );
-
-		$content = $dom->createElement( 'div' );
-		$content->setAttribute( 'class', 'payment_box_wrap cfw-radio-reveal-content-wrap' );
-
-		// Create title bar containers
-		$label_text = $dom->createElement( 'span' );
-		$label_text->setAttribute( 'class', 'payment_method_title cfw-radio-reveal-title' );
-
-		$payment_method_icon_container = $dom->createElement( 'span' );
-		$payment_method_icon_container->setAttribute( 'class', 'payment_method_icons' );
-
-		$title_label = $dom->createElement( 'label' );
-		$title_label->setAttribute( 'class', 'payment_method_label cfw-radio-reveal-label' );
-
-		$payment_selector = $this->get_element( $inputs, $klarna_payment_method_pattern, 'id' );
-
-		$klarna_payment_label = $this->get_element( $labels, $klarna_payment_method_pattern, 'for' );
-		$payment_box          = $this->get_element( $divs, $klarna_payment_box_pattern, 'class' );
-		$payment_box->setAttribute( 'class', $payment_box->getAttribute( 'class' ) . ' cfw-radio-reveal-content' );
-
-		for ( $i = 0; $i < $klarna_payment_label->childNodes->count(); $i++ ) {
-			$label_child_node = $klarna_payment_label->childNodes->item( $i );
-
-			if ( $label_child_node->nodeName == 'a' ) {
-				$label_child_node->removeAttribute( 'style' );
-			}
-
-			$label_text->appendChild( $label_child_node );
-		}
-
-		// Append the title label items
-		$title_label->appendChild( $label_text );
-
-		// Append the payment icons
-		for ( $i = 0; $i < $payment_method_icons->count(); $i++ ) {
-			$payment_method_icon_container->appendChild( $payment_method_icons->item( $i ) );
-		}
-
-		// Append the title label and payment method icon container
-		$title_bar->appendChild( $title_label );
-		$title_bar->appendChild( $payment_method_icon_container );
-
-		$content->appendChild( $payment_box );
-
-		// Append payment method title bar and content
-		$li->appendChild( $payment_selector );
-		$li->appendChild( $title_bar );
-		$li->appendChild( $content );
-
-		$klarna_payment_label->parentNode->removeChild( $klarna_payment_label );
-
-	}
-
-	/**
-	 * @param \DOMNodeList $elements
-	 * @param string $pattern
-	 * @param string $attribute
-	 * @param boolean $recursive
-	 *
-	 * @return \DOMElement
-	 */
-	function get_element( $elements, $pattern, $attribute, $recursive = false ) {
-		for ( $i = 0; $i < $elements->count(); $i++ ) {
-			$element = $elements->item( $i );
-
-			// Skip text nodes
-			if ( $element->nodeType != XML_ELEMENT_NODE || ! $element->hasAttribute( $attribute ) ) {
-				continue;
-			}
-
-			d( "Node Name: {$element->nodeName}| Data Attribute: $attribute | Has Attribute: {$element->hasAttribute($attribute)} | Has Children: {$element->hasChildNodes()}" );
-
-			if ( preg_match( $pattern, $element->getAttribute( $attribute ) ) ) {
-				return ( $recursive && $element->hasChildNodes() ) ? $this->get_element( $element->childNodes, $pattern, $attribute, true ) : $element;
+								echo apply_filters("cfw_payment_gateway_field_html_{$gateway->id}", $field_html);
+								?>
+							</div>
+						</div>
+					<?php endif; ?>
+				</li>
+				<?php
 			}
 		}
-	}
-
-	function get_klarna_template() {
-		ob_start();
-		echo wc_get_template( 'checkout/payment-method.php', [ 'gateway' => $this->klarna_payments ] );
-		$klarna_template = ob_get_contents();
-		ob_clean();
-
-		return $klarna_template;
 	}
 }

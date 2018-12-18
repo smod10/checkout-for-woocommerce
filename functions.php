a52c54291a281ccc159b28f9dbf81ec44a52f44f
Clifton Griffin <clifgriffin@gmail.com>
2018-12-17 22:53:22 -0500

    Spacing.

--- functions.php
+++ functions.php
@@ -587,7 +587,7 @@ if ( ! function_exists( 'woocommerce_form_field' ) ) {
 	    $wc_notices = wc_print_notices( $return = true );
 
 	    if ( empty($wc_notices) ) return;
-	    
+
 	    echo "<div id='cfw-wc-print-notices' class='cfw-alert cfw-alert-danger' style='display:block;'><div class='message'>$wc_notices</div></div>";
     }
 }

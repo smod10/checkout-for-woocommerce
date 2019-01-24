74e7d94492f7326b2aa1ecd1bcdc8ce8d082d775
Clifton Griffin <clifgriffin@gmail.com>
2019-01-24 10:20:31 -0500

    Suppress add to cart notices on checkout when using WooCommerce's native add-to-cart URL parameter.
    I really didn't want to re-order the hooks but WC's default hook provides absolutely no context, so it's impossible to reliably determine if we are on the checkout page.

--- includes/Compatibility/Plugins/WooCommerceCore.php
+++ includes/Compatibility/Plugins/WooCommerceCore.php
@@ -9,6 +9,24 @@ class WooCommerceCore extends Base {
 		return true; // always on, baby
 	}
 
+	public function pre_init() {
+		add_action( 'wp_loaded', array($this, 'move_add_to_cart_action'), 0 );
+		add_filter('wc_add_to_cart_message_html', array($this, 'suppress_add_to_cart_notices'), 1 ); // run this late
+	}
+
+	function move_add_to_cart_action() {
+		remove_action( 'wp_loaded', array( 'WC_Form_Handler', 'add_to_cart_action' ), 20 );
+		add_action( 'wp', array( 'WC_Form_Handler', 'add_to_cart_action' ), 10, 0 );
+	}
+
+	function suppress_add_to_cart_notices( $notice ) {
+		if ( is_checkout() && ! empty($_REQUEST['add-to-cart']) ) {
+			return '';
+		} else {
+			return $notice;
+		}
+	}
+
 	public function remove_scripts( $scripts ) {
 		$scripts['woocommerce'] = 'woocommerce';
 

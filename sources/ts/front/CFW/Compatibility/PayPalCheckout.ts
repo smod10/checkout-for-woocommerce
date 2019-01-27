0e814c0be1d90ebfa1ce613096f47c0f669fc441
Clifton Griffin <clifgriffin@gmail.com>
2019-01-27 09:40:50 -0500

    Handle issue of PayPal buttons displaying inconsistently.

--- sources/ts/front/CFW/Compatibility/PayPalCheckout.ts
+++ sources/ts/front/CFW/Compatibility/PayPalCheckout.ts
@@ -1,4 +1,6 @@
 import { Compatibility } from "./Compatibility";
+import {Main} from "../Main";
+import {EasyTabDirection, EasyTabService} from "../Services/EasyTabService";
 
 declare let jQuery: any;
 
@@ -11,7 +13,19 @@ export class PayPalCheckout extends Compatibility {
         super(params, load);
     }
 
-    load(): void {
+    load(main: Main): void {
+        let easyTabsWrap: any = main.easyTabService.easyTabsWrap;
+
+        easyTabsWrap.bind('easytabs:after', (event, clicked, target) => {
+            let easyTabDirection: EasyTabDirection = EasyTabService.getTabDirection(target);
+            let payment_tab_id = main.tabContainer.tabContainerSectionBy("name", "payment_method").jel.attr("id");
+            let current_tab_id = EasyTabService.getTabId(easyTabDirection.target);
+
+            if ( payment_tab_id == current_tab_id ) {
+                jQuery(document.body).trigger( 'updated_checkout' );
+            }
+        });
+
         jQuery(window).on('load updated_checkout', () => {
             if ( jQuery('#woo_pp_ec_button_checkout').is(':visible') ) {
                 var isPPEC = jQuery( '#payment_method_ppec_paypal' ).is(':checked');

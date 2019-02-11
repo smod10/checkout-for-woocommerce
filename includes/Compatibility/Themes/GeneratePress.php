7a50d33139a2ea47aefd98a18f7aa3f644558e4f
Clifton Griffin <clifgriffin@gmail.com>
2019-02-11 08:05:41 -0500

    Remove scripts from GeneratePress in a way that we can detect properly if we are on the checkout page.

--- includes/Compatibility/Themes/GeneratePress.php
+++ includes/Compatibility/Themes/GeneratePress.php
@@ -10,7 +10,13 @@ class GeneratePress extends Base {
 	}
 
 	public function run() {
-		if ( ! is_checkout() ) return;
+		add_action( 'wp', array( $this, 'remove_gp_scripts' ) );
+	}
+
+	function remove_gp_scripts() {
+		if ( ! is_checkout() ) {
+			return;
+		}
 
 		remove_action( 'wp_enqueue_scripts', 'generatepress_wc_scripts', 100 );
 	}

cb67db4b43c43623d7af2c6fc210a60ebad0bf64
Clifton Griffin <clifgriffin@gmail.com>
2019-02-13 14:20:21 -0500

    Fix bug where WooCommerce Germanized nuked the order button.

--- includes/Compatibility/Plugins/WooCommerceGermanized.php
+++ includes/Compatibility/Plugins/WooCommerceGermanized.php
@@ -16,5 +16,12 @@ class WooCommerceGermanized extends Base {
 	public function run() {
 		add_action( 'cfw_checkout_before_payment_method_tab_nav', 'woocommerce_gzd_template_render_checkout_checkboxes' );
 		add_action( 'cfw_checkout_before_payment_method_tab_nav', 'woocommerce_gzd_template_checkout_set_terms_manually' );
+
+		/**
+		 * Don't let WooCommerce Germanized Eff Up the Submit Button
+		 */
+		remove_action( 'woocommerce_review_order_before_submit', 'woocommerce_gzd_template_set_order_button_remove_filter', PHP_INT_MAX );
+		remove_action( 'woocommerce_review_order_after_submit', 'woocommerce_gzd_template_set_order_button_show_filter', PHP_INT_MAX );
+		remove_action( 'woocommerce_gzd_review_order_before_submit', 'woocommerce_gzd_template_set_order_button_show_filter', PHP_INT_MAX );
 	}
 }
